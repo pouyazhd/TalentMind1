6652efdf36411d57b4d77306e0e7e1bd814d93b9
Pouya <p.zahedi@live.com>
2022-04-21 08:34:57 +0430

    openning dataset module compleated

--- preprocessing/opendataset.py
+++ preprocessing/opendataset.py
@@ -8,3 +8,37 @@ E-Mail: p.zahedi@live.com
 """
 
 import logging
+from sys import exit
+
+try:
+    from pandas import read_csv, DataFrame
+except Exception as err:
+    logging.error(err)
+    exit(1)
+
+
+class load_data:
+    """load dataset"""
+    def __init__(self, filename:str) -> DataFrame:
+        # extract file type
+
+        # open dataset
+        self.dataset= self.open_csv(filename=filename)
+        pass
+
+    def open_csv(self, filename:str)-> DataFrame:
+        """oped csv file dataset usuing pandas
+
+        Args:
+            filename (str): address of the file to open
+
+        Returns:
+            DataFrame: data frame of loaded dataset
+        """
+
+        try:
+            loaded_dataset = read_csv(filename)
+            return loaded_dataset
+        except Exception as err:
+            logging.error(err)
+            raise "Error in reading dataset. read logs for more info."
